3ef24a6c7e2c1746adb4f3127486f847911f4413
doyindev1 <ade.doyin27@gmail.com>
2023-08-14 12:02:34 +0100

    test

---
+++ App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.dummyText}>Hello World!!!!</Text>
+      <Text style={styles.dummyText}>Hello World!!!!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  dummyText: {
+    borderWidth: 16,
+    padding: 16,
+    borderColor: "green"
+  }
+});
